c496f9b606f8e5f719fb70f4f1638ff11787c3f0
yuzchen7 <yuzchen7@gmail.com>
2023-07-01 04:21:04 +0800

    feat : add router nav

--- src/APP/App.js
+++ src/APP/App.js
@@ -1,23 +1,29 @@
-import logo from '../logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+//3 mian pages
+import Home from "../pages/Home";
+import Students from "../pages/Students";
+import Campuses from "../pages/Campuses";
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav>
+          <center>
+            <Link to="/">Home</Link>
+            <br />
+            <Link to="/students">Students</Link>
+          </center>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/students" element={<Students />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
