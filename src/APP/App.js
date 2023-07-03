a56032cb1bb08bdea1d767a66848d97dbd656464
yuzchen7 <yuzchen7@gmail.com>
2023-07-03 23:16:44 +0800

    feat : add all campuses view router

--- src/APP/App.js
+++ src/APP/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "../pages/Home";
 import Students from "../pages/Students";
 import Campuses from "../pages/Campuses";
+import CampusesSingleView from "../pages/CampusesSingleView";
 
 function App() {
   return (
@@ -16,12 +17,16 @@ function App() {
             <Link to="/">Home</Link>
             <br />
             <Link to="/students">Students</Link>
+            <br />
+            <Link to="/campuses">Campuses</Link>
           </center>
         </nav>
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/students" element={<Students />} />
+          <Route path="/campuses" element={<Campuses />} />
+          <Route path="/campuses_single_view" element={<CampusesSingleView />} />
         </Routes>
       </Router>
     </div>
