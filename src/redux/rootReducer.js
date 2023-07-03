c371e9a83212d0a2fe0e0abfbccb05ce1dc049a7
yuzchen7 <yuzchen7@gmail.com>
2023-07-03 23:24:08 +0800

    feat : register campus redux

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import studentsReducer from "./students/students.reducer";
+import campusesReducer from "./campuses/campuses.reducer";
 
 const rootReducer = combineReducers({
     students: studentsReducer,
+    campuses: campusesReducer
 });
 
 export default rootReducer;
