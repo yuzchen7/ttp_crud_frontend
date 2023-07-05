66def16f7d6fda388b11db1e191eebf71894b474
yuzchen7 <yuzchen7@gmail.com>
2023-07-06 06:28:48 +0800

    refactor : change btn position

--- src/components/campus/CampusItemList.jsx
+++ src/components/campus/CampusItemList.jsx
@@ -29,12 +29,14 @@ export default function CampusItemList(props) {
                             Number of Students : {campus.studentsNum} <br />
                         </p>
                     </span>
-                    <Link to='/campus_single_view' state={campus.id} >
-                        <button>Single View</button>
-                    </Link>
                     <Link to='/campus_edit' state={campus}>
                         <button>Edit</button>
                     </Link>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <Link to='/campus_single_view' state={campus.id} >
+                        <button>Single View</button>
+                    </Link>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
                     <button id='deleteBtn' onClick={(e) => {e.preventDefault(); deleteCampus(campus.id)}}>Delete</button>
                     <hr />
                 </div>
